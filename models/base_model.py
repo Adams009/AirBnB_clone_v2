956b1ba6983fc7ba00fa018251b965a428f29703
Adams009 <olaadam003@gmail.com>
2024-03-20 11:13:07 -0700

    go

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,75 @@
 #!/usr/bin/python3
-"""Module base_model
-
-The Module of BaseModel Class
+"""
+Contains class BaseModel
 """
 
-import uuid
 from datetime import datetime
-
 import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
-    """BaseModel Class"""
+    """The BaseModel class"""
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """method & instantiation of Basemodel
-
-        Args:
-            *args.
-            **kwargs (dict): Key/value pairs
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if kwargs is not None and len(kwargs) > 0:
-            for k, v in kwargs.items():
-                if k == "__class__":
-                    continue
-                elif k in ["created_at", "updated_at"]:
-                    setattr(self, k, datetime.fromisoformat(v))
-                else:
-                    setattr(self, k, v)
+        """Initialization of the base model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """String representation of class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        """Update updated_at with current datetime."""
-        self.updated_at = datetime.now()
+        """updates 'updated_at' to current datetime"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary of __dict__ copy
-        """
-        bs_dict = (
-            {
-                k: (v.isoformat() if isinstance(v, datetime) else v)
-                for (k, v) in self.__dict__.items()
-            }
-        )
-        bs_dict["__class__"] = self.__class__.__name__
-        return bs_dict
+        """returns a dictionary"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
 
-    def __str__(self) -> str:
-        """print str representation of BaseModel."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def delete(self):
+        """delete from the storage"""
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python3
-"""Module base_model
+#!/usr/bin/python
+"""  class of City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-This Module contains City Class
-"""
 
-from models.base_model import BaseModel
+class City(BaseModel, Base):
+    """Rep of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
-
-class City(BaseModel):
-    """A class that city
-
-    Attributes:
-        name (str): name of city
-        state_id (str): state id
-    """
-
-    state_id = ""
-    name = ""
+    def __init__(self, *args, **kwargs):
+        """init of city"""
+        super().__init__(*args, **kwargs)
